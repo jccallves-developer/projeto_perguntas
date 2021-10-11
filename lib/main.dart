dd6c555a2d26fdab39d214b9c6b59fcab3f097cd
jccallves <jccallves@macae.ufrj.br>
2021-10-11 12:18:52 -0300

    #Aula37

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,45 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(new PerguntaApp());
+  runApp(PerguntaApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita',
+      'Qual é o seu animal favorito'
+    ];
+
     return MaterialApp(
-      home: Text('Olá Flutter!'),
+      home: Scaffold(
+        appBar: AppBar(
+          // ignore: prefer_const_constructors
+          title: Text('Perguntas'),
+        ),
+        // ignore: prefer_const_constructors
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
